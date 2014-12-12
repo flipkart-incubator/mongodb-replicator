2a56bdfb3606da649de3cb0de7948dc08701fa3a
Kishan Gajjar <kishan.gajjar@flipkart.com>
2014-12-12 11:07:25 +0530

    adding unittest for mongoConnector

---
+++ mongo-node-discovery/src/test/java/flipkart/mongo/node/discovery/test/MongoConnectorTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2012-2015, the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package flipkart.mongo.node.discovery.test;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoURI;
+import flipkart.mongo.node.discovery.MongoConnector;
+import flipkart.mongo.node.discovery.exceptions.ConnectionException;
+import org.junit.Assert;
+
+import java.net.UnknownHostException;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by kishan.gajjar on 12/12/14.
+ */
+public class MongoConnectorTest extends BaseDiscoveryTest {
+
+    private MongoURI mockMongoURI;
+    private Mongo mockMongoClient;
+
+    @Override
+    public void setUp() throws Exception {
+        mockMongoURI = mock(MongoURI.class);
+        mockMongoClient = mock(Mongo.class);
+        MongoConnector mongoConnector = new MongoConnector();
+    }
+
+    public void testMongoClient() throws Exception {
+        when(mockMongoURI.connect()).thenReturn(mockMongoClient);
+        Mongo mongoClient = MongoConnector.getMongoClient(mockMongoURI);
+
+        Assert.assertTrue("MongoClient assertion", mockMongoClient == mongoClient);
+    }
+
+    public void testMongoClientException() throws Exception{
+        when(mockMongoURI.connect()).thenThrow(UnknownHostException.class);
+
+        try {
+            MongoConnector.getMongoClient(mockMongoURI);
+        } catch (Exception e) {
+            Assert.assertSame("ConnectionException assertion", e.getClass(), ConnectionException.class);
+        }
+    }
+}
