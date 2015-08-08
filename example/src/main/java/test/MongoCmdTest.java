b2a89076fca94b6b446fc2c76a142be7cdae738c
Kishan Gajjar <kishan.gajjar@flipkart.com>
2015-08-08 13:11:21 +0530

    updating versions

---
+++ example/src/main/java/test/MongoCmdTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2012-2015, the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package test;
+
+import flipkart.mongo.replicator.core.model.Node;
+
+/**
+ * Created by kishan.gajjar on 15/07/15.
+ */
+public class MongoCmdTest {
+    private static final String DB_FOR_DISCOVERY = "admin";
+
+    public static void main(String[] args) {
+
+        Node node = new Node("cart-mongo3.nm.flipkart.com", 26102);
+//        MongoClient client = MongoConnector.getMongoClient(node.getMongoURI());
+//
+//        MongoDatabase database = client.getDatabase(DB_FOR_DISCOVERY);
+//
+//        BasicDBObject replSetGetStatusCommand = new BasicDBObject("replSetGetStatus", 1);
+//        Document replSetGetStatus = database.runCommand(replSetGetStatusCommand);
+//        System.out.println(" REPLCI STATUS" + replSetGetStatus);
+    }
+}
